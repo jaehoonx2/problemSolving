cbfffdc724f0a35252dcb6819990aa6dd7bb6736
Jaehoon Jung <iamjungjaehoon@gmail.com>
2023-09-03 16:12:25 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int oddNum = 0;
+        int evenNum = 0;
+        
+        for(int i = 0; i < num_list.length; i++){
+            if(num_list[i]%2 == 0)
+                evenNum++;
+            else
+                oddNum++;
+        }
+        
+        int[] answer = {evenNum, oddNum};
+        
+        return answer;
+    }
+}
