be17bbabc9c0acea0061a315af85ffe781b38d6e
Jaehoon Jung <iamjungjaehoon@gmail.com>
2023-09-04 23:52:52 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int n) {
+        int l = (n % 2 == 0) ? n/2 : n/2 + 1;
+        int[] answer = new int[l];
+        
+        for(int i = 1; i <= l; i++)
+            answer[i-1] = 2*i - 1;
+        
+        return answer;
+    }
+}
