622b6157df7af6a070bb5907452c7d2a72297b39
Jaehoon Jung <iamjungjaehoon@gmail.com>
2023-09-05 00:02:24 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+
+class Solution {
+    public int solution(int n) {
+        for(int i = 1; i <= n; i++)
+            if(i*i == n)
+                return 1;
+        
+        return 2;
+    }
+}
