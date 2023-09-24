da5cc26f42bf558f017961c6266896ccdefdbcea
Jaehoon Jung <iamjungjaehoon@gmail.com>
2023-09-24 12:57:46 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.02 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] solution(int[] num_list, int n) {
+        int rowNum = num_list.length / n;
+        int[][] answer = new int[rowNum][n];
+        
+        for(int i = 0; i < num_list.length; i++){
+            int row = i / n;
+            int col = i % n;
+            
+            answer[row][col] = num_list[i];
+        }
+
+        return answer;
+    }
+}
