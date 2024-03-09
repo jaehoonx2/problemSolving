fa015d4f7cbe6c9937d155a29f8746e257c7572c
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-03-09 18:38:48 +0900

    [level 0] Title: 문자열 뒤집기, Time: 11.66 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,11 @@
+import java.lang.StringBuilder;
+
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        StringBuilder sb = new StringBuilder(my_string.substring(s, e+1));
+        
+        return my_string.substring(0, s) +
+               sb.reverse().toString() +
+               my_string.substring(e+1, my_string.length());
+    }
+}
