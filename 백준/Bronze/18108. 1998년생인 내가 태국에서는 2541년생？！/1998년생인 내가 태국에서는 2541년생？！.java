de8cbb45df55f20051c2f5eee26650c45a9d31da
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-03-11 16:12:52 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        int dif = 2541 - 1998;
+        
+        System.out.println(y - dif);
+    }
+}
