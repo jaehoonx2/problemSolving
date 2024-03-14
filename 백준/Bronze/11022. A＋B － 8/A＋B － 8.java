e1411a54eeb013340dde3fee9652634d80f0071e
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-03-14 14:14:48 +0900

    [Bronze V] Title: A+B - 8, Time: 180 ms, Memory: 16464 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int t = Integer.parseInt(br.readLine());
+        for(int i = 0; i < t; i++){
+            String[] str = br.readLine().split(" ");
+            int a = Integer.parseInt(str[0]);
+            int b = Integer.parseInt(str[1]);
+            int c = a + b;
+            bw.write("Case #" +
+                     Integer.toString(i+1) +
+                     ": " +
+                     str[0]
+                     + " + " + 
+                     str[1] +
+                     " = " +
+                     Integer.toString(c));
+            
+            if(i < t-1)
+                bw.newLine();
+        }
+        
+        bw.flush();
+    }
+}
