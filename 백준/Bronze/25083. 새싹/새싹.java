a3fb2c97b81f1e8afbb1351a12efb603dd40be3e
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-03-31 16:53:22 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 13900 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Main {
+	public static void main (String[] args) throws java.lang.Exception {
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");	
+		System.out.println("      |");			
+	}
+}
