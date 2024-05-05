865f23099bbf1db8404017fe2c027874a96c92cb
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-05 14:17:48 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,22 +1,24 @@
 SELECT *
-FROM
-(
-    SELECT
-    TO_CHAR(A.SALES_DATE, 'YYYY-MM-DD') AS SALES_DATE
-    ,A.PRODUCT_ID
-    ,A.USER_ID
-    ,A.SALES_AMOUNT
-    FROM ONLINE_SALE A
-    WHERE TO_CHAR(A.SALES_DATE, 'YYYYMM') = '202203'
+  FROM
+        (
+            SELECT DATE_FORMAT(SALES_DATE, '%Y-%m-%d') SALES_DATE,
+                   PRODUCT_ID,
+                   USER_ID,
+                   SALES_AMOUNT
+              FROM ONLINE_SALE
+             WHERE 1=1
+               AND YEAR(SALES_DATE) = 2022
+               AND MONTH(SALES_DATE) = 03
 
-    UNION ALL
+            UNION ALL
 
-    SELECT
-    TO_CHAR(B.SALES_DATE, 'YYYY-MM-DD') AS SALES_DATE
-    ,B.PRODUCT_ID
-    ,NULL AS USER_ID
-    ,B.SALES_AMOUNT
-    FROM OFFLINE_SALE B
-    WHERE TO_CHAR(B.SALES_DATE, 'YYYYMM') = '202203'
-) K
-ORDER BY K.SALES_DATE ASC, PRODUCT_ID ASC, USER_ID ASC;
+            SELECT DATE_FORMAT(SALES_DATE, '%Y-%m-%d') SALES_DATE,
+                   PRODUCT_ID,
+                   NULL USER_ID,
+                   SALES_AMOUNT
+              FROM OFFLINE_SALE
+             WHERE 1=1
+               AND YEAR(SALES_DATE) = 2022
+               AND MONTH(SALES_DATE) = 03
+        ) A
+ ORDER BY A.SALES_DATE, A.PRODUCT_ID, A.USER_ID;
