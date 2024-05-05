a36602ddea65e2cf65682ecaa7418a4337af0b9a
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-05 14:51:18 +0900

    [level 1] Title: 12세 이하인 여자 환자 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
@@ -0,0 +1,10 @@
+SELECT PT_NAME,
+       PT_NO,
+       GEND_CD,
+       AGE,
+       IFNULL(TLNO, 'NONE') TLNO
+  FROM PATIENT
+ WHERE 1=1
+   AND AGE <= 12
+   AND GEND_CD = 'W'
+ ORDER BY AGE DESC, PT_NAME ASC
