f6ab620fe92e5de693a235785420badcd0003690
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-06 16:05:43 +0900

    [level 2] Title: 행렬의 곱셈, Time: 1.40 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int m = arr1.length;
+        int k = arr2.length; 
+        int n = arr2[0].length;
+        int[][] answer = new int[m][n];
+        
+        for(int i = 0; i < m; i++)
+            for(int j = 0; j < n; j++)
+                for(int l = 0; l < k; l++)
+                    answer[i][j] += arr1[i][l] * arr2[l][j];   
+
+        return answer;
+    }
+}
