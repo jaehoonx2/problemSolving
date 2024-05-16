b8bb1de655c00b2dd6bd58d7ec00e8ec5b74a02e
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-16 17:23:54 +0900

    [Bronze IV] Title: 기찍 N, Time: 1156 ms, Memory: 33056 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Main {
+	public static void main (String[] args) throws java.lang.Exception {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for(int i = N; i >= 1; i--)
+			System.out.println(i);
+	}
+}
