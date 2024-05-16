79123403dac965dbc59ead9a5395d73791bf84b6
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-16 20:53:26 +0900

    [Bronze V] Title: 콜센터, Time: 120 ms, Memory: 13860 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args) throws java.lang.Exception {
+		System.out.println("     /~\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _\\=/_");
+		System.out.println("   /  _  \\");
+		System.out.println("  //|/.\\|\\\\");
+		System.out.println(" ||  \\ /  ||");
+		System.out.println("============");	
+		System.out.println("|          |");	
+		System.out.println("|          |");	
+		System.out.println("|          |");			
+	}
+}
