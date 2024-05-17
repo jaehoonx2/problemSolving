636123f182150b70842f0ad7ea52f8c9a8e02f75
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-17 17:40:45 +0900

    [Bronze V] Title: CAPS, Time: 532 ms, Memory: 31268 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args) throws java.lang.Exception {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.println(str.toUpperCase());
+	}
+}
