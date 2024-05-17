418cf03cad502ea66f5b098044810081a490c135
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-17 12:41:35 +0900

    [Bronze V] Title: 연세대학교, Time: 196 ms, Memory: 17380 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args) throws java.lang.Exception {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		if(a == 0)
+			System.out.println("YONSEI");
+		else
+			System.out.println("Leading the Way to the Future");
+	}
+}
