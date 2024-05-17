33ccbd45a2c4bd80dde0c2e822cc441af77ac1af
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-17 11:24:50 +0900

    [Unrated] Title: 더하기, Time: 204 ms, Memory: 17380 KB -BaekjoonHub

---
+++ 백준/Unrated/14918. 더하기/더하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args) throws java.lang.Exception {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println((a+b));
+	}
+}
