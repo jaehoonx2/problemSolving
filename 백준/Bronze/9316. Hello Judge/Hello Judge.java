8eb1ab950ed27ce0c0f49201824511e1bdadd029
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-17 12:39:58 +0900

    [Bronze IV] Title: Hello Judge, Time: 240 ms, Memory: 18272 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args) throws java.lang.Exception {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		for(int i = 1; i <= a; i++)
+			System.out.println("Hello World, Judge " + i + "!");
+	}
+}
