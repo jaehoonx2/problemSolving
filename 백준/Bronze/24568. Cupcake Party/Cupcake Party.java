233070c93ceb3553598175d68dd0c8fac8f5cb4a
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-20 11:30:53 +0900

    [Bronze V] Title: Cupcake Party, Time: 208 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args) throws java.lang.Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		int r = sc.nextInt();
+		int s = sc.nextInt();
+		
+		System.out.println((r*8 + s*3 - 28));
+	}
+}
