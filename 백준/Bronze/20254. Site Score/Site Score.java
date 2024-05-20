c6cbda54ebd8c1ccb4ec94a31447b091244936d2
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-20 11:16:18 +0900

    [Bronze V] Title: Site Score, Time: 204 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args) throws java.lang.Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt() * 56;
+		int b = sc.nextInt() * 24;
+		int c = sc.nextInt() * 14;
+		int d = sc.nextInt() * 6;
+        
+		System.out.println((a+b+c+d));
+	}
+}
