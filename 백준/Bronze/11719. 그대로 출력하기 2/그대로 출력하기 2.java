bbd2ac688242baeb6af54baa1538a86141947591
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-21 15:06:16 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 236 ms, Memory: 17956 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args) throws java.lang.Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()){
+			String str = sc.nextLine();
+			System.out.println(str);
+		}		
+	}
+}
