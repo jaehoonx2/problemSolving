df15cc79aa0c4a3ac9e45375a076067ae0271180
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-22 15:54:16 +0900

    [Bronze I] Title: A/B - 2, Time: 220 ms, Memory: 18060 KB, Score: 18 point -BaekjoonHub

---
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
+
+class Main {
+	public static void main (String[] args) throws java.lang.Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		
+		System.out.println((a / b));
+	}
+}
