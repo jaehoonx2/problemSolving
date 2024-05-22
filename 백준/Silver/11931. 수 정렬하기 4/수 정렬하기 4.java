05160c778f939058c59e6439b65cba9f52d267e4
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-22 15:23:06 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 2840 ms, Memory: 322160 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Main {
+	public static void main (String[] args) throws java.lang.Exception {
+		Scanner sc = new Scanner(System.in);
+ 
+		int n = sc.nextInt();
+ 
+		int[] arr = new int[n];
+ 
+		for(int i = 0; i < arr.length; i++)
+			arr[i] = sc.nextInt();
+ 
+		Arrays.sort(arr);
+        
+        StringBuilder sb = new StringBuilder();
+		
+        for(int i = arr.length - 1; i >= 0; i--)
+			sb.append(Integer.toString(arr[i]) + '\n');
+        
+        System.out.println(sb.toString());
+	}
+}
