a4f25eaac69f770e6ff20624ddf4cc4878c97b10
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-27 19:53:58 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 220 ms, Memory: 18312 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.next();
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::"+id+"::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
