850e9c196f6bb93a3dc3135a2d9009deda36ff9a
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-27 19:45:15 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 6084 ms, Memory: 397160 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.BigInteger;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = new BigInteger(sc.next());
+        BigInteger b = new BigInteger(sc.next());
+
+        System.out.println(a.multiply(b));
+    }
+}
