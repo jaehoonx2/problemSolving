60ab7b2606b3afbaac3c5b183eb92a1fe476ae46
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-27 19:29:14 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 224 ms, Memory: 18308 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.BigInteger;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        num += 1;
+
+        System.out.println(num*2 + " " + num*3);
+    }
+}
