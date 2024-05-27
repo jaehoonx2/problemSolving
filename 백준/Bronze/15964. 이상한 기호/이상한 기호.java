a552bfd8d0d4b2b1b9bfff90863d4c94a4c64027
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-27 20:42:06 +0900

    [Bronze V] Title: 이상한 기호, Time: 208 ms, Memory: 17644 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+
+        long result = (a + b) * (a - b);
+
+        System.out.println(result);
+    }
+}
