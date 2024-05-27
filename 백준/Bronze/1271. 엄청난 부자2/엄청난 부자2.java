19e713f8d5d11222adca4412de4f1d359639ab8e
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-27 16:13:34 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 200 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.BigInteger;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        BigInteger bia = new BigInteger(a);
+        BigInteger bib = new BigInteger(b);
+
+        System.out.println(bia.divide(bib));
+        System.out.println(bia.mod(bib));
+    }
+}
