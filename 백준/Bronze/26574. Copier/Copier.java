97cecda8a3c67e4f2728609608f4854c1623054e
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-28 13:05:21 +0900

    [Bronze V] Title: Copier, Time: 228 ms, Memory: 18444 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while(sc.hasNext()){
+            int num = sc.nextInt();
+            System.out.println(num + " " + num);
+        }
+    }
+}
