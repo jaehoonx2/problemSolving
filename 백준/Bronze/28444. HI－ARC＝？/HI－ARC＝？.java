7ebb721a058d7f638af6e7a77805fdd6642828b9
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-29 10:21:58 +0900

    [Bronze V] Title: HI-ARC=?, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int i = sc.nextInt();
+        int a = sc.nextInt();
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println((h*i - a*r*c));
+    }
+}
