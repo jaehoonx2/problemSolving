a8bb8ca0f100f491ee11cbaedb30a99b9fd3a2b1
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-30 17:38:35 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 212 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt(); 
+        int k = sc.nextInt();
+        int l = sc.nextInt();
+        System.out.println(m * k + l);
+    }
+}
