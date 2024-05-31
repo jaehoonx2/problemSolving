157c24375355c46dd0200d868f2ca54068c305db
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-05-31 17:24:17 +0900

    [Bronze IV] Title: 특별한 가지, Time: 208 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        System.out.println(m/n*k);
+    }
+}
