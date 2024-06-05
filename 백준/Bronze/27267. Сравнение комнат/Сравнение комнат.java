d612cc438c2b12cfc423379addce0ec75237dca4
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-06-05 12:36:34 +0900

    [Bronze IV] Title: Сравнение комнат, Time: 208 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/27267. Сравнение комнат/Сравнение комнат.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        System.out.println(a*b > c*d ? "M" : a*b < c*d ? "P" : "E");
+    }
+}
