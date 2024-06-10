96e7ae3a9ef0d69829d8883ff9bd41e50ae65411
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-06-10 12:44:39 +0900

    [Bronze IV] Title: 10부제, Time: 216 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int day = sc.nextInt();
+        int cnt = 0;
+        while(sc.hasNext()){
+            int num = sc.nextInt();
+            cnt += day == num ? 1 : 0; 
+        }
+        System.out.println(cnt);        
+    }
+}
