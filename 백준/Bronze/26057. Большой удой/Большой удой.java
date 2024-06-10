ad67d552f3cb1b7fd51fb1f2153889f469210796
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-06-10 12:00:59 +0900

    [Bronze IV] Title: Большой удой, Time: 204 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/26057. Большой удой/Большой удой.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long l = sc.nextInt();
+        long t = sc.nextInt();
+        System.out.println(t - (l-t));
+    }
+}
