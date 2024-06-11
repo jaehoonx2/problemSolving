4e156bd063655f8d1111858f0e7f799b522ed22e
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-06-11 12:25:12 +0900

    [Bronze IV] Title: 입실 관리, Time: 216 ms, Memory: 17564 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i = 0; i < n; i++){
+            String str = sc.next();
+            System.out.println(str.toLowerCase());
+        }
+    }
+}
