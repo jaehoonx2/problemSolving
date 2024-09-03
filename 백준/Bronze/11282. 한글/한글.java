651abad32f87f105261f83ec02fe30e522d5b280
Jaehoon Jung <iamjungjaehoon@gmail.com>
2024-09-03 22:53:20 +0900

    [Bronze IV] Title: 한글, Time: 172 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/11282. 한글/한글.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println((char) (n + 44031));
+    }
+}
